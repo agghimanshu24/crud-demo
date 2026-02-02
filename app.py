7f89e0ad16ed3e1f358845d90c7f6bfc649198d6
Himanshu Aggarwal <himanshusmac@Himanshus-MacBook-Air.local>
2026-02-02 22:12:29 +0530

    commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def get_connection():
     return sql.connect(
         server_hostname="dbc-a49f6e9d-ba1d.cloud.databricks.com",
         http_path="/sql/1.0/warehouses/96c844134aa6f9ca",
-        access_token="dapi8a2353b4ad6f949ef4acea61f2f58877"
+        access_token="abc"
     )
 
 
